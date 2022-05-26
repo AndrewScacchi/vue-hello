4ca4a6bc63183065a1c4d924f27ee7e718490624
andrea scacchi <andrea.scacchi@live.com>
2022-05-26 15:19:30 +0200

    Final commit. AGAIN. this time will it works?

---
+++ script.js
@@ -0,0 +1,11 @@
+// first VUE script
+console.log("helloworld")
+const root = new Vue({
+    el : "#container",
+    data :  {
+        message : "Hello World!",
+        mini : "(but in vue)"
+    }
+})
+
+// okay remember to check what you copypaste gaaaaaaaah
